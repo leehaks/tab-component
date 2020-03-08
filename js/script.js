3626cec2411d600d9a7ceb8340e07d62531152f5
bugman001 <jhlee@bitnine.net>
2020-03-08 21:03:15 +0900

    first commit Tab Component

---
+++ js/script.js
@@ -0,0 +1,60 @@
+window.onload = function(){
+    
+    // tab-component
+
+    let currentTabLabel, currentTabPanel; 
+    let tabComponent = document.querySelectorAll('.tab-component'); 
+
+    function activate(elem){
+        elem.classList.add('active'); 
+        currentTabLabel = elem; 
+    }
+
+    function inactivate(elem){ 
+        elem.classList.remove('active'); 
+    }
+
+    function activatePanel(elem){
+        elem.classList.add('active'); 
+        currentTabPanel = elem; 
+    }
+    
+    function tabHandler(e) { 
+        const targetElem = e.target; 
+        const targetContain = targetElem.classList.contains('tab-label')
+
+        if (targetContain) { 
+            inactivate(currentTabLabel); 
+            inactivate(currentTabPanel); 
+
+            activate(targetElem);     
+            
+            let tabOffSetLeft = this.getBoundingClientRect().left.toFixed(2)
+
+            this.querySelector('.bar').style.left = 
+            targetElem.getBoundingClientRect().left.toFixed(2) - tabOffSetLeft +"px"; 
+            
+            console.log(tabOffSetLeft,targetElem.getBoundingClientRect().left.toFixed(2)); 
+            
+
+            let targetHref, targetPanel; 
+            targetHref = targetElem.getAttribute('href')
+            targetPanel = this.querySelector(targetHref); 
+            
+            activatePanel(targetPanel); 
+        }       
+    }
+
+    tabComponent.forEach(items => { 
+        let tabLabelFirst = items.querySelector('.tab-label-group .tab-label');
+        let tabPanelFirst = items.querySelector('.tab-panel');
+        activate(tabLabelFirst); 
+        activatePanel(tabPanelFirst);
+
+        let labelBar = document.createElement('div'); 
+        labelBar.classList.add('bar');
+        items.querySelector('.tab-label-group').appendChild(labelBar); 
+
+        items.addEventListener('click', tabHandler); 
+    })
+}
